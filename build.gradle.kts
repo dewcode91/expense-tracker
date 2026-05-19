ae3e0c6554df4d53cf1c7dc3a6898ea8d5a7f178
dewcode <49826484+dewcode91@users.noreply.github.com>
2026-05-19 18:56:13 +0530

    Fix buildscript repositories for Gradle classpath deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
